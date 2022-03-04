c5aa63e685b62de72b1c6bb166614d9dd083bbef
ediguerrero <edixon_guerrero96@hotmail.com>
2022-03-03 19:51:12 -0500

    fix instances

--- src/main/java/Notificator.java
+++ src/main/java/Notificator.java
@@ -2,27 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Notificator {
-  private final List<String> accounts;
-  private final TYPES types;
-  private List <INotification> notifications;
+    private final List<String> accounts;
+    private final TYPES types;
+    private List<INotification> notifications;
 
 
-    public Notificator( TYPES types,List<String> accounts ) {
+    public Notificator(TYPES types, List<String> accounts) {
         this.accounts = accounts;
         this.types = types;
-    }
-
-    public void enviar(String message){
-        notifications = new ArrayList<INotification>();
-        notifications .add(new FacebookImpl());
+        notifications = new ArrayList<>();
+        notifications.add(new FacebookImpl());
         notifications.add(new SMSImpl());
         notifications.add(new CorporateEmail());
+    }
 
-           for (INotification notification:notifications){
-              if( notification.validateType(types.name())){
-                  notification.send(message, accounts);
-
-           }
-       }
+    public void enviar(String message) {
+        for (INotification notification : notifications) {
+            if (notification.validateType(types.name())) {
+                notification.send(message, accounts);
+            }
+        }
     }
 }
